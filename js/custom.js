40032212d5e111c8bc38527c66adf30644ee7e89
emmanualtemp <emmanualtemp@gmail.com>
2025-04-17 23:21:09 +0530

    new changes

--- js/custom.js
+++ js/custom.js
@@ -1,15 +1,40 @@
-$(document).ready(function(){
-    loading ();
-    function loading (){
-         var fullText = "Oh, Hello There";
-            var currentIndex = 0;
-            function revealText() {
-                if (currentIndex <= fullText.length) {
-                    $('#loadingText').text(fullText.substring(0, currentIndex));
-                    currentIndex++;
-                    setTimeout(revealText, 150);
-                }
-            }
-            revealText();
-        }
-});
+$(document).ready(function () {
+  loading()
+
+  setTimeout(() => {
+    $('#loader').hide()
+  }, 3000)
+
+  function loading () {
+    var fullText = 'Oh, Hello There'
+    var currentIndex = 0
+    function revealText () {
+      if (currentIndex <= fullText.length) {
+        $('#loadingText').text(fullText.substring(0, currentIndex))
+        currentIndex++
+        setTimeout(revealText, 150)
+      }
+    }
+    revealText()
+  }
+
+  $('#sub-menu').click(function () {
+    $('#menu-scroll-view').slideToggle()
+    $('body').css('overflow', 'hidden')
+  })
+
+  $('.menu-scroll-view-inner').click(function () {
+    $('#menu-scroll-view').hide()
+    $('body').css('overflow', 'auto')
+  })
+
+  $(window).on('scroll', function () {
+    if ($(this).scrollTop() > 100) {
+      $('#sub-menu').addClass('show')
+      $('#navbarNav').addClass('hidden')
+    } else {
+      $('#sub-menu').removeClass('show')
+      $('#navbarNav').removeClass('hidden')
+    }
+  })
+})
