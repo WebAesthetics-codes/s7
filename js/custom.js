7eb34b11055b131cfc7ec53891684b9da7c1e94e
emmanualtemp <emmanualtemp@gmail.com>
2025-04-20 00:07:58 +0530

    new changes

--- js/custom.js
+++ js/custom.js
@@ -40,20 +40,113 @@ $(document).ready(function () {
 
   gsap.registerPlugin(ScrollTrigger)
 
-  gsap.to('.video-section', {
+  gsap.to('.video-section-inner', {
     scrollTrigger: {
       trigger: '.video-section',
-      start: 'top center',
-      end: 'bottom top',
+      start: 'top top',
+      end: '+=100%',
       scrub: true,
+      pin: true,
       markers: false
     },
     scale: 1,
     opacity: 1,
     width: '100vw',
     height: '100vh',
-    bottom: '0',
+    top: '0',
     transform: 'translateX(-50%)',
     ease: 'power3.out'
+  })
+
+  gsap.to('.center-content-section h2', {
+    scrollTrigger: {
+      trigger: '.center-content-section',
+      start: 'top 80%',
+      end: 'top 79%',
+      toggleActions: 'play none none reverse'
+    },
+    rotationX: 0,
+    opacity: 1,
+    duration: 1.2,
+    ease: 'power4.out'
+  })
+
+  gsap.to('.items-section-inner', {
+    scrollTrigger: {
+      trigger: '.items-section-view',
+      start: 'top top',
+      end: '+=100%',
+      scrub: true,
+      pin: true,
+      markers: false
+    },
+    scale: 1,
+    opacity: 1,
+    width: '100vw',
+    height: '100vh',
+    top: '0',
+    transform: 'translateX(-50%)',
+    ease: 'power3.out'
+  })
+
+  gsap.to('.items-section-inner-second', {
+    scrollTrigger: {
+      trigger: '.items-section-view-second',
+      start: 'top top',
+      end: '+=100%',
+      scrub: true,
+      pin: true,
+      markers: false
+    },
+    scale: 1,
+    opacity: 1,
+    width: '100vw',
+    height: '100vh',
+    top: '0',
+    transform: 'translateX(-50%)',
+    ease: 'power3.out'
+  })
+
+  gsap.to('.items-section-inner-third', {
+    scrollTrigger: {
+      trigger: '.items-section-view-third',
+      start: 'top top',
+      end: '+=100%',
+      scrub: true,
+      pin: true,
+      markers: false
+    },
+    scale: 1,
+    opacity: 1,
+    width: '100vw',
+    height: '100vh',
+    top: '0',
+    transform: 'translateX(-50%)',
+    ease: 'power3.out'
+  })
+
+  gsap.to('.items-section-inner-four', {
+    scrollTrigger: {
+      trigger: '.items-section-view-four',
+      start: 'top top',
+      end: '+=100%',
+      scrub: true,
+      pin: true,
+      markers: false
+    },
+    scale: 1,
+    opacity: 1,
+    width: '100vw',
+    height: '100vh',
+    top: '0',
+    transform: 'translateX(-50%)',
+    ease: 'power3.out'
+  })
+
+  gsap.to('.footer-text-section h2', {
+    xPercent: -50,
+    repeat: -1,
+    duration: 10,
+    ease: 'linear'
   })
 })
